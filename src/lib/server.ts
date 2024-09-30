1c94d9cdc55933cf69d023c9f4fb5d6f94bac3ef
Sigfredo <hello@sigfredo.fun>
2024-09-30 16:09:26 +0200

    lowered context size
    memory issues with raspberry pi

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -3,10 +3,12 @@ import path from 'node:path';
 
 const llama = await getLlama();
 const model = await llama.loadModel({
-	modelPath: path.join(process.cwd(), 'src', 'lib', 'models', 'llama-3.2-3b-instruct-q4_k_m.gguf')
+	modelPath: path.join(process.cwd(), 'src', 'lib', 'models', 'llama-3.2-3b-instruct-hf.gguf')
 });
 
-const context = await model.createContext();
+const context = await model.createContext({
+	contextSize: 10000
+});
 export const llamaChat = new LlamaChat({
 	contextSequence: context.getSequence()
 });
